d3a82abf4360835947667b846a08f37204230787
Aryan Chauhan <aryanchau1516@gmail.com>
2024-09-19 23:49:49 +0530

    Update DuplicateEle.java

--- ARRAYS/DuplicateEle.java
+++ ARRAYS/DuplicateEle.java
@@ -1,21 +1,39 @@
-package ARRAYS;
-public class DuplicateEle {  
-    public static void main(String[] args) {      
-       
-        int [] arr = new int [] {1, 2, 3, 4, 5, 6, 8, 7, 13};   
-      
-       System.out.println( dupli(arr));
-    }
-   static int dupli(int arr[])  {
-    for(int i = 0; i < arr.length; i++) {  
-        for(int j = i + 1; j < arr.length; j++) {  
-            if(arr[i] == arr[j])  
-               return arr[i]; 
-             
-        }  
-    }  
+import java.util.HashSet;
 
+public class RemoveDuplicates {
+    public static int[] removeDuplicates(int[] arr) {
+        // Use two HashSets
+        HashSet<Integer> uniqueSet = new HashSet<>();  // To track unique elements
+        HashSet<Integer> duplicateSet = new HashSet<>();  // To track duplicates
 
-        return -1 ;
+        // Process the array
+        for (int num : arr) {
+            if (!uniqueSet.add(num)) {
+                // If element is already in uniqueSet, it's a duplicate
+                duplicateSet.add(num);
+            }
+        }
+
+        // Remove all duplicates from uniqueSet
+        uniqueSet.removeAll(duplicateSet);
+
+        // Convert uniqueSet back to array
+        int[] result = new int[uniqueSet.size()];
+        int i = 0;
+        for (int num : uniqueSet) {
+            result[i++] = num;
+        }
+
+        return result;
     }
-}  
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 4, 1, 5};
+        int[] uniqueArr = removeDuplicates(arr);
+        
+        // Print the result (only elements that appear once)
+        for (int num : uniqueArr) {
+            System.out.print(num + " ");
+        }
+    }
+}

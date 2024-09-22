23f1a45ceb58b11fab76bf71a7d71ac1aab136ab
Aryan Chauhan <aryanchau1516@gmail.com>
2024-09-22 23:22:13 +0530

    Add files via upload
    /*
     * hello aryan chauhan
       nahuahc nayra olleh
     */
    
    it program revrse a stack completly in  0(n) time complexity

---
+++ stackk/REerse.java
@@ -0,0 +1,28 @@
+ import java.util.Stack;
+/*
+ * hello aryan chauhan
+   nahuahc nayra olleh
+ */
+public class REerse {
+    public static void main(String[] args) {
+        String str = "hello aryan chauhan";
+        System.out.println(str);
+       System.out.println( reverseString(str));
+    }
+    public static String reverseString(String str)
+    {
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx<str.length()) {
+            s.push(str.charAt(idx));   
+            idx ++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+            
+        }
+        return result.toString();
+    }
+}

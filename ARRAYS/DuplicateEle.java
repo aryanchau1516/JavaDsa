0488c5827bfe49e2d72bb285a7a3cc294eb642ab
Aryan Chauhan <aryanchau1516@gmail.com>
2024-09-24 21:55:09 +0530

    Update DuplicateEle.java

--- ARRAYS/DuplicateEle.java
+++ ARRAYS/DuplicateEle.java
@@ -1,39 +1,24 @@
+package ARRAYS;
+
 import java.util.HashSet;
 
-public class RemoveDuplicates {
-    public static int[] removeDuplicates(int[] arr) {
-        // Use two HashSets
-        HashSet<Integer> uniqueSet = new HashSet<>();  // To track unique elements
-        HashSet<Integer> duplicateSet = new HashSet<>();  // To track duplicates
-
-        // Process the array
-        for (int num : arr) {
-            if (!uniqueSet.add(num)) {
-                // If element is already in uniqueSet, it's a duplicate
-                duplicateSet.add(num);
-            }
-        }
-
-        // Remove all duplicates from uniqueSet
-        uniqueSet.removeAll(duplicateSet);
-
-        // Convert uniqueSet back to array
-        int[] result = new int[uniqueSet.size()];
-        int i = 0;
-        for (int num : uniqueSet) {
-            result[i++] = num;
-        }
-
-        return result;
+public class DuplicateEle {  
+    public static void main(String[] args) {      
+       
+        int [] arr = new int [] {1, 2, 3, 2, 5, 6, 8, 5, 13};   
+      
+       System.out.println( duplicate(arr));
     }
-
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 2, 4, 1, 5};
-        int[] uniqueArr = removeDuplicates(arr);
+   static HashSet<Integer> duplicate(int arr[]){
+        HashSet<Integer> st = new HashSet<>();
+        HashSet<Integer> gt = new HashSet<>();
+        for (Integer num :arr) {
+            
         
-        // Print the result (only elements that appear once)
-        for (int num : uniqueArr) {
-            System.out.print(num + " ");
+        if(!st.add(num)){
+            gt.add(num);
         }
-    }
-}
+        }
+        return gt;
+   }
+}  

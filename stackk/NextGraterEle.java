cd48ccaaaab5665f430880777c7d86892df27c7e
Aryan Chauhan <aryanchau1516@gmail.com>
2024-09-25 23:17:40 +0530

    Add files via upload

---
+++ stackk/NextGraterEle.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class NextGraterEle {
+    public static void main(String[] args) {
+        int arr[] = {8,1,0,5,2,3};
+        Stack<Integer> st = new Stack<>();
+
+        int nextGraterEle[] = new int[arr.length];
+        for(int i =arr.length-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]<=arr[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){      
+                nextGraterEle[i] = -1;
+            }
+            else{
+                nextGraterEle[i] = arr[st.peek()];  
+            }
+
+            st.push(i); // insert element in stack
+        }
+
+        for(int i =0 ;i<nextGraterEle.length;i++){
+            System.out.print(nextGraterEle[i] + " ");
+        }
+    }
+}

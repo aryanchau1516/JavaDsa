afe9850c98d8ea002e00982cb873daa7f4c16bec
Aryan Chauhan <aryanchau1516@gmail.com>
2024-09-26 00:56:35 +0530

    Update Balance_bracket.java

--- stackk/Balance_bracket.java
+++ stackk/Balance_bracket.java
@@ -3,77 +3,45 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Balance_bracket
+public class Balance_bracket  // or valid parantheiseish
  {
+            public static boolean validBracket(String str){
+                Stack<Character> st = new Stack<>();
+                for(int i =0; i<str.length();i++){
+                    char ch = str.charAt(i);
+                    if(ch=='('||ch=='{'||ch=='['){  // for opening
+                        st.push(ch);
+                    }
+                    else{  // for closeing bracket
+                        if (st.isEmpty()) {  // it means st does not have openning brackets its only have closing brackts its mean invalid parantheshish  )}
+                            return false;
+                            
+                        }
+                        if((st.peek()=='('&& ch == ')')
+                        || (st.peek()=='{'&& ch == '}') || (st.peek()=='['&& ch == ']')){
+                            st.pop();  // when bracket are like this () || {} || {}
+                           
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                }
 
+                if(st.isEmpty()){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
     public static void main(String[] args)
      {  Balance_bracket obj = new Balance_bracket();
         Stack<Character> st = new Stack<>();
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        for(int i=0;i<str.length();i++)
-        {
-            char ch = str.charAt(i);
-            if(ch=='('||ch=='{'||ch=='[')
-            {
-                st.push(ch);
-            }
-            else if(ch==')')
-            {
-               boolean val= obj.handleclosing(st,'(');
-               if(val==false)
-               {
-                System.out.println(val);
-                return;
-               }
-            }
-            else if(ch=='}')
-            {
-                boolean val=obj.handleclosing(st,'{');
-                if(val==false)
-                {
-                 System.out.println(val);
-                 return;
-                }
-            }
-             else if(ch==']')
-            {
-                boolean val= obj.handleclosing(st,'[');
-                if(val==false)
-                {
-                 System.out.println(val);
-                 return;
-                }
-            }
-        }
-        if(st.size()==0)
-        {
-            System.out.println(true);
-        }
-        else{
-            System.out.println(false);
-        }
-    }
-
-
-
-    public boolean  handleclosing(Stack<Character> st , char corresoch)
-    {
-        
-            if(st.size()==0)
-            {
-                //System.out.println(false);
-                return false;
-            }
-        
-        else if(st.peek()!=corresoch)
-        {
-           // System.out.println(false);
-            return false;
-        }
-        else{
-            st.pop();
-            return true;
-        }
-    }
+      //  Scanner sc = new Scanner(System.in);
+       // String str = sc.nextLine();
+      // String str = "(){{()}}";  // true
+       String str = "(){{([]])}}"; // false
+       System.out.println(validBracket(str)); 
+     }
  }

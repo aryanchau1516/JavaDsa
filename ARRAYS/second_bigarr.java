a7142a7a9d6c4295e84e06023a14904d0576a6cd
Aryan Chauhan <aryanchau1516@gmail.com>
2024-09-25 23:36:02 +0530

    Update second_bigarr.java

--- ARRAYS/second_bigarr.java
+++ ARRAYS/second_bigarr.java
@@ -3,22 +3,30 @@ public class second_bigarr {
     
         public static void main(String[] args) {
             int arr[] = new int[] {8,-8,0,-5,7,10,-9};
-           // int arr[] = new int[] {20,34,23,67,110,56,89};
-            int big=-(Integer.MAX_VALUE);
-            int big2=-(Integer.MAX_VALUE);
-            for(int i=0;i<arr.length;i++){
-    if(arr[i]>big)
-    {
-       //big=arr[i];
-       big2=big;
-       big=arr[i];
-    }
-    else if(arr[i]>big2&&arr[i]<big)
-    {
-        big2=arr[i];
-    }}
-    // System.out.println("biggest element of the array :"+big);
-            System.out.println("second largest number is :"+big2);
+            if (arr.length < 2) {
+            //throw new IllegalArgumentException("Array must have at least two elements.");
+             return -1;
+        }
+
+        int big1 = Integer.MIN_VALUE;
+        int big2 = Integer.MIN_VALUE;
+
+        // Iterate over the array using a for loop
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > big1) {
+                big2 = big1;  //  second the previous largest
+                big1 = arr[i];  //  first  the new largest
+            } else if (arr[i] > big2 && arr[i] != big1) {
+                big2 = arr[i];  
+            }
+        }
+
+        // If second is not updated, that means there is no second largest element
+        if (big2 == Integer.MIN_VALUE) {
+            return -1;
+        }
+
+        return big2;
     
         }
         

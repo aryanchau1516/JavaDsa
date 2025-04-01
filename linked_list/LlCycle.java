03bcdb23c1e1528915342a3ace0666d93893fcd7
Aryan Chauhan <aryanchau1516@gmail.com>
2025-04-01 22:02:30 +0530

    Add files via upload

---
+++ linked_list/LlCycle.java
@@ -0,0 +1,18 @@
+package linked_list;
+
+public class LlCycle {
+    public class Node {
+       int data;
+       Node next;
+       Node(int data){
+        this.data=data;
+        this.next=null;
+       }
+        
+    }
+    Node head=null;
+    Node tail =null;
+    public static void main(String[] args) {
+       // Node head = new Node(1);
+    }
+}
